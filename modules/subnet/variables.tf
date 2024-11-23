af6d2fe82fe10516a7605e5d47d460eb1f717f65
Pritamsingh230 <146520739+Pritamsingh230@users.noreply.github.com>
2024-11-23 23:00:27 +0530

    Update variables.tf

--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -3,7 +3,19 @@ variable "vpc_id" {
   type        = string
 }
 
-variable "subnet_name1" {
-  description = "subnet names"
+variable "subnet_name" {
+  description = "Base name for the subnets"
   type        = string
+}
+
+variable "public_cidr_block" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24" # Default public subnet CIDR
+}
+
+variable "private_cidr_block" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24" # Default private subnet CIDR
 }
