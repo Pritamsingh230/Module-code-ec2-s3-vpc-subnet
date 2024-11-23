7c811908dc9bb1a2ef07775547f3a4b67590801c
Pritamsingh230 <146520739+Pritamsingh230@users.noreply.github.com>
2024-11-23 23:17:17 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,8 @@
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
 variable "vpc_name" {
   description = "The name of the VPC"
   type        = string
