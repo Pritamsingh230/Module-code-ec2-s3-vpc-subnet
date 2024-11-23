f9bab305d5a643de9ffcd4aac28000d305a95b64
Pritamsingh230 <146520739+Pritamsingh230@users.noreply.github.com>
2024-11-24 00:17:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,68 @@
-variable "vpc_name" {
-  description = "The name of the VPC"
-  type        = string
-  default = "vpc23"
+variable "terra_ami_id" {
+  description = "The AMI ID to use for the EC2 instance"
+  default     = "ami-0866a3c8686eaeeba"
+
+  type = string
 }
+variable "instance_name1" {
+  description = "The name tag for the EC2 instance"
+  default     = "instance1"
+  type        = string
+}
+variable "instance_typee" {
+  description = "Instance type"
+  default     = "t2.micro"
+  type        = string
+} 
+
+variable "bucket_name2" {
+  description = "The name of S3 buckets"
+  type = string
+  default = "my-bucket-instance-s453"
+}
+
+
+
+# variable "vpc_name1" {
+#   description = "The name of the VPC"
+#   type        = string
+#   default = "my-vpc1"
+# }
+
+
+
+
+
+
+variable "vpc_name" {
+  description = "Name for the VPC"
+  type        = string
+  default     = "my-vpc1"
+}
+
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
 
 variable "subnet_name" {
-  description = "Name of the subnets"
+  description = "Base name for the subnets"
   type        = string
-  default = "subnet12"
+  default     = "subnet"
 }
 
-variable "region" {
-  description = "Region to deploy resources"
+variable "public_cidr_block" {
+  description = "CIDR block for the public subnet"
   type        = string
-  default     = "us-east-1"
+  default     = "10.0.1.0/24"
 }
 
+variable "private_cidr_block" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
 
