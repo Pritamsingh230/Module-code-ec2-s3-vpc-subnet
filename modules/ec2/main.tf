29503a57a43239f47094a3a19ca8742807e76376
Pritamsingh230 <146520739+Pritamsingh230@users.noreply.github.com>
2024-11-23 22:07:03 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2" {
   ami           = var.ami_id             
   instance_type = var.instance_type     
 
-  tags = {
-    Name = "MyEC2Instance"
+   tags = {
+    Name = var.instance_name
   }
 }
