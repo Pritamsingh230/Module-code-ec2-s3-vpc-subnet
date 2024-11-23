6a37f1f68248278422bacad531256cbd8ebd6a2a
Pritamsingh230 <146520739+Pritamsingh230@users.noreply.github.com>
2024-11-23 23:17:06 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,7 @@
-resource "aws_vpc" "vpc1" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
     Name = var.vpc_name
